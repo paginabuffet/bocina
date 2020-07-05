d40ef6f2182c74a08752f074d098ed51b6bc3194
paginabuffet <67081576+paginabuffet@users.noreply.github.com>
2020-07-05 00:54:11 -0300

    Add files via upload

---
+++ app/sw.js
@@ -0,0 +1,31 @@
+
+// Necesitamos usar Polyfill porque todavía no todos los navegadores 
+// son compatibles con la API de caché
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.html?homescreen=1',
+       '/?homescreen=1',
+       '/styles/main.css',
+       '/scripts/main.min.js',
+       '/sounds/airhorn.mp3'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open('airhorner')
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
