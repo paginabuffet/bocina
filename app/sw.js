59d2340237767f1f211e91e6c5e76106af188f6b
paginabuffet <67081576+paginabuffet@users.noreply.github.com>
2020-07-05 02:19:20 -0300

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -9,11 +9,6 @@ self.addEventListener('install', function(e) {
    caches.open('airhorner').then(function(cache) {
      return cache.addAll([
        '/index.html',
-       '/index.html?homescreen=1',
-       '/?homescreen=1',
-       '/styles/main.css',
-       '/scripts/main.min.js',
-       '/sounds/airhorn.mp3',
      ]);
    })
  );
