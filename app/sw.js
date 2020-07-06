f252bfb16748c09916135dc1ddcd19b2724ec77b
paginabuffet <67081576+paginabuffet@users.noreply.github.com>
2020-07-05 22:49:43 -0300

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -8,7 +8,7 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('airhorner').then(function(cache) {
      return cache.addAll([
-       '',
+       '/',
        'index.html',
        'index.html?homescreen=1',
        '?homescreen=1',
