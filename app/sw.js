00acabacc433bed4399a533d8528cf8f69fd4da6
paginabuffet <67081576+paginabuffet@users.noreply.github.com>
2020-07-06 02:54:04 -0300

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -17,12 +17,13 @@ self.addEventListener('install', function(e) {
      ]);
    })
  );
- self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+/*
+  self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
 });
+*/
 
 self.addEventListener('fetch', event => {
   event.respondWith(
